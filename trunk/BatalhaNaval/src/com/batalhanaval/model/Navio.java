57889541b22cc5390926ede6c3545e72f037f1b7
reiinoldo@gmail.com <reiinoldo@gmail.com@ab4867c0-ae3f-473f-b2b7-303c4f48372d>
2014-11-20 22:56:56 +0000

    Navios

---
+++ trunk/BatalhaNaval/src/com/batalhanaval/model/Navio.java
@@ -0,0 +1,21 @@
+package com.batalhanaval.model;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Navio extends View {
+
+	public Navio(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Navio(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+		
+}
