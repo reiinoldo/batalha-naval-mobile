adebe2bb92212e77527f753dbcf8c07db3d603d8
reiinoldo <reiinoldo@gmail.com>
2014-11-20 22:56:56 +0000

    Navios

--- BatalhaNaval/src/com/example/batalhanaval/MainActivity.java
+++ BatalhaNaval/src/com/example/batalhanaval/MainActivity.java
@@ -1,16 +1,52 @@
 package com.example.batalhanaval;
 
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Gallery;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
+	
+	private LinearLayout ll;
+	private ImageButton ibt1; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_main);
+		
+		ibt1 = (ImageButton) findViewById(R.id.ibt1);
+		ibt1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				ibt1.setImageResource(R.drawable.ship1);
+				
+			}
+		});
+//				
+//		ll = new LinearLayout(this);
+//		
+//		ImageView i = new ImageView(this);
+//	
+//		i.setImageResource(R.drawable.ship1);
+//        i.setAdjustViewBounds(true); // set the ImageView bounds to match the Drawable's dimensions
+//		i.setLayoutParams(new Gallery.LayoutParams(LayoutParams.WRAP_CONTENT,
+//		LayoutParams.WRAP_CONTENT));
+//	
+//		// Add the ImageView to the layout and set the layout as the content view		
+//		ll.addView(i);		
+//		setContentView(ll);
+		
+	  
 	}
 
 	@Override
@@ -31,4 +67,8 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	
+	
+	
 }
